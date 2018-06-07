dd6b32c86ce7699eade4b3ba882cb987fd2fd160
dgcode <dgjorgievski@live.com>
2018-06-07 16:30:12 +0200

    Add post stage

--- src/test/java/demo/example/SimpleTestProject/apitests/CheckResponseSpeedApiIT.java
+++ src/test/java/demo/example/SimpleTestProject/apitests/CheckResponseSpeedApiIT.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 
-public class CheckResponseSpeedApiTest {
+public class CheckResponseSpeedApiIT {
 	
 	@BeforeClass
 	public void initPath() {
